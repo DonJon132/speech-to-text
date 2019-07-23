993f360dfc2210af60ab8b27d085f709f7fc15b5
Jon Dalton Tillo <jon@symph.co>
2019-07-23 14:03:09 +0800

    v2 working

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
-const speechRecog = window.speechRecog || window.webkitspeechRecog;
-const recog = new speechRecog();
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
 
-recog.onstart = function(){
-    console.log('voice is activated');
+
+recognition.onstart = function(){
+    console.log('voice is activated');  
 };
 
-recog.onresult = function(event){
+recognition.onresult = function(event){
     const current = event.resultIndex;
     const transcript = event.results[current][0].transcript;
     content.textContent = transcript;
-    readOutLoud(transcript);
+    ReadOutLoud(transcript);
 };
 
 
 btn.addEventListener('click', () => {
-    recog.start();
+    recognition.start();
 });
+
+function ReadOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = message;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    
+    window.speechSynthesis.speak(speech);
+}
